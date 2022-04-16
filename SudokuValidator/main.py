c7e4008c383fd5cbf39a8399405368131e2037f0
arkadiPiven <arkadi7770@gmail.com>
2022-04-16 23:42:56 +0300

    Added Somefunctions Added Sudoku solver

--- SudokuValidator/main.py
+++ SudokuValidator/main.py
@@ -1,21 +1,95 @@
-class Job:
-    def execute(self):
-        return
+import threading
 
 
-class Row(Job):
-    def execute(self):
-        return
+class Validator:
+    scheduler = []
+    threads = []
+    num_of_threads = 1
+    solution = None
+    total_jobs_done = 0
+    lock = threading.Lock()
+    all_jobs_done = threading.Condition()
+    scheduler_empty = threading.Condition()
 
+    def __init__(self, solution, num_of_threads):
+        self.solution = solution
+        self.num_of_threads = num_of_threads
+        for i in range(self.num_of_threads):
+            self.threads.append(threading.Thread(target=self.get_job(), args=()))
+            self.threads[i].start()
 
-class Column(Job):
-    def execute(self):
-        return
+    def get_job(self):
+        while True:
+            self.lock.acquire()
+            while not self.scheduler:
+                self.scheduler_empty.wait()
+            job = self.scheduler.pop(0)
+            self.lock.release()
+            ret_value = job.execute()
+            if ret_value == 1:
+                return
 
+    def schedule_job(self, job):
+        self.lock.acquire()
+        self.scheduler.append(job)
+        if len(self.scheduler) == 1:
+            self.scheduler_empty.notify()
+        self.lock.release()
 
-class Block(Job):
-    def execute(self):
-        return
+    def solve(self):
+        for i in range(0, 9):
+            self.schedule_job(self.Row(i))
+            self.schedule_job(self.Column(i))
+            self.schedule_job(self.Block(i))
+        for i in range(self.num_of_threads):
+            self.schedule_job(self.Cyanide())
+        for thread in self.threads:
+            thread.join()
+
+    class Job:
+        start_index = 0
+
+        def __init__(self, start_index):
+            self.start_index = start_index
+
+        def execute(self):
+            return
+
+    class Row(Job):
+
+        def __init__(self, start_index):
+            super().__init__(start_index)
+
+        def execute(self):
+            lst = []
+            for j in range(9):
+                if self.solution[i][j] in lst:
+                    return False
+
+    class Column(Job):
+
+        def __init__(self, start_index):
+            super().__init__(start_index)
+
+        def execute(self):
+            return
+
+    class Block(Job):
+
+        def __init__(self, start_index):
+            super().__init__(start_index)
+
+        def execute(self):
+            return
+
+    class Cyanide:
+
+        def __init__(self):
+            return
+
+        @staticmethod
+        def execute():
+            return 1
 
 
 def valid_solution(solution):
